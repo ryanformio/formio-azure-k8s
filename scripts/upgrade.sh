29d87cc55c22ef9633f1a83535f144ea220c2b6c
ryanformio <ryan@form.io>
2023-03-10 21:00:37 -0600

    PDF works forms work but downloading is being funky

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -28,10 +28,12 @@ helm -n $NAMESPACE upgrade -i --debug --wait --atomic \
 --set dbSecret=$DB_SECRET \
 --set jwtSecret=$JWT_SECRET \
 --set pdfServer=$PDF_SERVER \
-# --set formioS3Key=$FORMIO_S3_KEY \
-# --set formioS3Secret=$FORMIO_S3_SECRET \
-# --set formioS3Bucket=$FORMIO_S3_BUCKET \
-# --set formioS3Server=$FORMIO_S3_SERVER \
+--set formioAzureContainer=$FORMIO_AZURE_CONTAINER \
+--set forimoAzureConnectionString=$FORMIO_AZURE_CONNECTION_STRING \
+--set formioS3Key=$FORMIO_S3_KEY \
+--set formioS3Secret=$FORMIO_S3_SECRET \
+--set formioS3Bucket=$FORMIO_S3_BUCKET \
+--set formioS3Server=$FORMIO_S3_SERVER \
 formio $PATH_TO_CHART
 
 echo ""
